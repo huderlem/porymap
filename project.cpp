4ecdaa0617a34160f2bd6a3f81e8b21abc639c26
yenatch <yenatch@gmail.com>
2018-01-29 21:43:38 -0500

    field_map_obj.c moved

--- project.cpp
+++ project.cpp
@@ -652,7 +652,7 @@ void Project::loadObjectPixmaps(QList<Event*> objects) {
     QString pointers_text = readTextFile(root + "/include/data/field_map_obj/map_object_graphics_info_pointers.h");
     QString info_text = readTextFile(root + "/include/data/field_map_obj/map_object_graphics_info.h");
     QString pic_text = readTextFile(root + "/include/data/field_map_obj/map_object_pic_tables.h");
-    QString assets_text = readTextFile(root + "/src/field_map_obj.c");
+    QString assets_text = readTextFile(root + "/src/field/field_map_obj.c");
 
     QStringList pointers = readCArray(pointers_text, "gMapObjectGraphicsInfoPointers");
 
