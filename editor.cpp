f227805b7f8b3da259b57b92e77df4c4ff5885f8
garakmon <thomastaps194@comcast.net>
2018-06-25 10:25:39 -0400

    fixing build errors
    Are these only present on non-Windows computers?

--- editor.cpp
+++ editor.cpp
@@ -2,6 +2,7 @@
 #include <QCheckBox>
 #include <QPainter>
 #include <QMouseEvent>
+#include <math.h>
 
 Editor::Editor(Ui::MainWindow* ui)
 {
@@ -249,7 +250,7 @@ void Editor::setConnectionEditControlsEnabled(bool enabled) {
     ui->spinBox_ConnectionOffset->setEnabled(enabled);
 
     if (!enabled) {
-        setConnectionEditControlValues(false);
+        setConnectionEditControlValues(0);
     }
 }
 
