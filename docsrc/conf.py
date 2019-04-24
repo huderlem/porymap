cd56aeef678e94f247f4abb3050075911e6941ad
garak <garakmon@gmail.com>
2019-04-24 10:00:17 -0400

    add changelog and recommonmark req

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -14,8 +14,6 @@
 #
 # import os
 # import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -38,8 +36,7 @@ release = u''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,8 +44,13 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+
+from recommonmark.parser import CommonMarkParser
+
+source_suffix = ['.rst', '.md']
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # The master toctree document.
 master_doc = 'index'
@@ -80,7 +82,13 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+#html_logo = '../resources/icons/porymap-icon-1.ico'
+
+html_theme_options = {
+    'style_nav_header_background': 'linear-gradient(180deg, #08ACD5 50%, #FF6262 0%);',
+    'logo_only': False,
+    'collapse_navigation': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -97,6 +105,9 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
+# Custom stylesheet
+def setup(app):
+    app.add_stylesheet('css/custom.css')
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
