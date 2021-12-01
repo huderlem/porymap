45a90bb01761f2cfca8edb9c6a3420d8b9582e32
GriffinR <griffin.g.richards@gmail.com>
2021-12-01 16:06:14 -0500

    Add tile/metatile to image functions to API, more tile->tileId/metatileId

--- include/ui/imageproviders.h
+++ include/ui/imageproviders.h
@@ -9,6 +9,7 @@
 QImage getCollisionMetatileImage(Block);
 QImage getCollisionMetatileImage(int, int);
 QImage getMetatileImage(uint16_t, Tileset*, Tileset*, QList<int>, QList<float>, bool useTruePalettes = false);
+QImage getMetatileImage(Metatile*, Tileset*, Tileset*, QList<int>, QList<float>, bool useTruePalettes = false);
 QImage getTileImage(uint16_t, Tileset*, Tileset*);
 QImage getPalettedTileImage(uint16_t, Tileset*, Tileset*, int, bool useTruePalettes = false);
 QImage getGreyscaleTileImage(uint16_t tile, Tileset *primaryTileset, Tileset *secondaryTileset);
