12dba1a8b3283b5a150d57707a4494fae3b9724c
GriffinR <griffin.g.richards@gmail.com>
2024-12-17 22:02:46 -0500

    Add Qt version to About, remove changelog

--- src/ui/aboutporymap.cpp
+++ src/ui/aboutporymap.cpp
@@ -1,22 +1,21 @@
 #include "aboutporymap.h"
 #include "ui_aboutporymap.h"
-#include "log.h"
 
 AboutPorymap::AboutPorymap(QWidget *parent) :
-    QMainWindow(parent),
+    QDialog(parent),
     ui(new Ui::AboutPorymap)
 {
     ui->setupUi(this);
     setAttribute(Qt::WA_DeleteOnClose);
 
-    QString versionInfo = QString("Version %1 - %2").arg(QCoreApplication::applicationVersion()).arg(QStringLiteral(__DATE__));
-
     static const QString commitHash = PORYMAP_LATEST_COMMIT;
-    if (!commitHash.isEmpty())
-        versionInfo.append(QString("\nCommit %1").arg(commitHash));
-
-    this->ui->label_Version->setText(versionInfo);
-    this->ui->textBrowser->setSource(QUrl("qrc:/CHANGELOG.md"));
+    this->ui->label_Version->setText(QString("Version %1%2\nQt %3 (%4)\n%5")
+                                        .arg(QCoreApplication::applicationVersion())
+                                        .arg(commitHash.isEmpty() ? "" : QString(" (%1)").arg(commitHash))
+                                        .arg(QStringLiteral(QT_VERSION_STR))
+                                        .arg(QSysInfo::buildCpuArchitecture())
+                                        .arg(QStringLiteral(__DATE__))
+                                    );
 }
 
 AboutPorymap::~AboutPorymap()
