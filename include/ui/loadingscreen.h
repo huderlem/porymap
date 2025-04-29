7426f474711d903c8a559232e241589a92f5e4e1
garak <garakmon@gmail.com>
2025-04-29 15:59:53 -0400

    clean up

--- include/ui/loadingscreen.h
+++ include/ui/loadingscreen.h
@@ -4,17 +4,11 @@
 #include <QTimer>
 #include <QWidget>
 
+
+
 namespace Ui {
 class LoadingScreen;
 }
-
-// Loading class
-// LOAD() // or Offload() OFFLOAD() WorkerThread()
-// function that wraps around QFuture and QtConcurrent, does not block gui thread
-// can call any other function that is not directly painting the gui on another thread
-
-// Execute function while playing loading screen and display text
-// void executeLoadingScreen(QString text, void (*function)(void));
 
 class PorymapLoadingScreen : public QWidget {
     
